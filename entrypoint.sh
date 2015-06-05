6e979c765d2c359a4b4164066401bed43cdf0632
Manasi Kulkarni <mkulkarn@thoughtworks.com>
2015-06-05 12:17:26 +0100

    removed sudo access

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ssh-keygen -N "" -f /var/go/.ssh/id_rsa
-sudo echo -e "Host * \n   StrictHostKeyChecking no" > $HOME/.ssh/config
-sudo service go-server start
+echo "Host * \n   StrictHostKeyChecking no" > $HOME/.ssh/config
+service go-server start
